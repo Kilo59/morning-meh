6c5b428be96d2a19bded0321086ec072cfb24810
Gabriel <gabriel59kg@gmail.com>
2019-10-19 18:13:40 -0400

    basic scrape

--- meh_txt/__main__.py
+++ meh_txt/__main__.py
@@ -2,17 +2,17 @@ import os
 from twilio.rest import Client
 
 # Twilio Config
-ACCOUNT_SID = os.environ.get('ACCOUNT_SID')
-AUTH_TOKEN = os.environ.get('AUTH_TOKEN')
-FROM_NUMBER = os.environ.get('FROM_NUMBER')
-TO_NUMBER = os.environ.get('TO_NUMBER')
+ACCOUNT_SID = os.environ.get("ACCOUNT_SID")
+AUTH_TOKEN = os.environ.get("AUTH_TOKEN")
+FROM_NUMBER = os.environ.get("FROM_NUMBER")
+TO_NUMBER = os.environ.get("TO_NUMBER")
 
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
-msg = client.messages.create(
-                from_=FROM_NUMBER,
-                body="This message is meh...",
-                to=TO_NUMBER)
+
+def send(msg):
+    return client.messages.create(from_=FROM_NUMBER, body=msg, to=TO_NUMBER)
+
 
 if __name__ == "__main__":
-    print(msg)
+    print(__doc__)
---
+++ meh_txt/scraper.py
@@ -0,0 +1,44 @@
+import contextlib
+import requests as r
+import bs4
+
+
+def is_good_response(resp):
+    """
+    Returns True if the response seems to be HTML, False otherwise.
+    """
+    content_type = resp.headers["Content-Type"].lower()
+    return (
+        resp.status_code == 200
+        and content_type is not None
+        and content_type.find("html") > -1
+    )
+
+
+def simple_get(url):
+    """
+    Attempts to get the content at `url` by making an HTTP GET request.
+    If the content-type of response is some kind of HTML/XML, return the
+    text content, otherwise return None.
+    """
+    try:
+        with contextlib.closing(r.get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            return None
+    except r.exceptions.RequestException as e:
+        print(f"Error during requests to {url} : {e}")
+        return None
+
+
+MEH_URL = "https://meh.com"
+
+raw_html = simple_get(MEH_URL)
+html = bs4.BeautifulSoup(raw_html, "html.parser")
+article = html.select("article")
+h2 = html.select("h2")
+features = html.find("section", class_="features")
+
+
+if __name__ == "__main__":
+    print(features)
