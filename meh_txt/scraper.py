65032a1ff7283180dffc94b85f4dd6fbd4ca644e
Gabriel <gabriel59kg@gmail.com>
2020-01-29 22:24:49 -0500

    name and price

--- meh_txt/scraper.py
+++ meh_txt/scraper.py
@@ -1,8 +1,34 @@
+"""
+meh_text.scraper.py
+~~~~~~~~~~~~~~~~~~~
+"""
+import logging
+import pathlib
 import gazpacho as gzp
 
+LGGR = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
 MEH_URL = "https://meh.com"
-html = gzp.get(MEH_URL)
+
+cache_file = pathlib.Path("cache.html")
+if cache_file.exists():
+    LGGR.debug("using cache")
+    html = cache_file.read_text()
+else:
+    LGGR.debug("building cache")
+    html = gzp.get(MEH_URL)
+    cache_file.write_text(html)
+
 soup = gzp.Soup(html)
 features = soup.find("section", {"class": "features"})
+product_text = features.find("h2").text
+name, price = product_text.split("$")
+name = name.replace("-", "").strip()
+price = price.strip()
+print(name)
 if __name__ == "__main__":
-    print(features)
+    print(features.attrs)
+    print(features.tag)
+    print(name)
+    print(price)
